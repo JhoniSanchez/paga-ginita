cd4363746f6753708736d7f1747019ec754f79e5
JhoniSanchez <jhonimanuel7184.js@gmail.com>
2024-06-03 16:14:06 -0400

    Pagina en Git

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,24 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+// import { defineConfig } from "vite"
+// import react from "@vitejs/plugin-react"
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:3003",
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3003",
-        changeOrigin: true,
-      },
-    },
-  },
-})
+  base: '/paga-ginita/', // Reemplaza <repositorio> con el nombre de tu repositorio
+});
